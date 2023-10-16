67a3d5c0bef0d424a0598e0b0959fc3b730cf8eb
6310210304 <6310210304@psu.ac.th>
2023-10-16 22:36:17 +0700

    backend success

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -8,23 +8,21 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.model.Faculty;
 import com.example.demo.model.Location;
-import com.example.demo.repository.FacultyRepository;
 import com.example.demo.repository.LocationRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class LocationController {
 	
--- src/main/java/com/example/demo/controller/PictureController.java
+++ src/main/java/com/example/demo/controller/PictureController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.example.demo.model.Picture;
 import com.example.demo.repository.PictureRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PictureController {
 	
